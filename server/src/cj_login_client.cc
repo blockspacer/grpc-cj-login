2dcef5b0ed8f1886dd2816f45702db773d7c85c3
jeasonzhu <jeasonzhu@tencent.com>
2018-08-05 00:48:17 +0800

    [server]client test

--- server/src/cj_login_client.cc
+++ server/src/cj_login_client.cc
@@ -31,32 +31,28 @@ public:
   CjLoginClient(std::shared_ptr<Channel> channel)
     : stub_(CjLoginService::NewStub(channel)) {}
 
-  Status registerUser(string username,
-                      string password) {
-    RegisterUserRequest req;
-    req.set_username(username);
-    req.set_password(password);
-
-    RegisterUserResponse res;
+  Status registerUser(RegisterUserRequest &req,
+                      RegisterUserResponse *resp) {
     ClientContext context;
-    auto status = stub_->registerUser(&context, req, &res);
-    std::cout << res.baseresp().errcode() << res.baseresp().errmsg() << std::endl;
-    return status;
+    return stub_->registerUser(&context, req, resp);
   }
 
-  Status userLogin(string username, string password) {
-    UserLoginRequest req;
-    req.set_username(username);
-    req.set_password(password);
-
-    UserLoginResponse resp;
+  Status login(const UserLoginRequest &req,
+                   UserLoginResponse *resp) {
     ClientContext context;
-    auto status = stub_->userLogin(&context, req, &resp);
-    std::cout << resp.baseresp().errcode()
-              << resp.baseresp().errmsg()
-              << resp.loginticket()
-              << std::endl;
-    return status;
+    return stub_->userLogin(&context, req, resp);
+  }
+
+  Status checkLogin(const UserCheckLoginRequest &req,
+                    UserCheckLoginResponse *resp) {
+    ClientContext context;
+    return stub_->userCheckLogin(&context, req, resp);
+  }
+
+  Status logout(const UserLogoutRequest &req,
+                UserLogoutResponse *resp) {
+    ClientContext context;
+    return stub_->userLogout(&context, req, resp);
   }
 
 private:
@@ -67,8 +63,12 @@ int main(int argc, char *argv[]) {
   CjLoginClient client(CreateChannel("localhost:50051",
                                      InsecureChannelCredentials()));
 
+  if (argc != 4) {
+    return 0;
+  }
+
   int i = 1;
-  string v = string(argv[i]);
+  string v(argv[i]);
   if (v == "-f") {
     string func(argv[i + 1]);
 
@@ -76,11 +76,46 @@ int main(int argc, char *argv[]) {
     if (func == "register") {
       string userName(argv[i + 2]);
       string password(argv[i + 3]);
-      status = client.registerUser(userName, password);
+
+      RegisterUserRequest req;
+      RegisterUserResponse resp;
+      req.set_username(userName);
+      req.set_password(password);
+
+      status = client.registerUser(req, &resp);
     } else if (func == "login") {
       string userName(argv[i + 2]);
       string password(argv[i + 3]);
-      status = client.userLogin(userName, password);
+
+      UserLoginRequest req;
+      UserLoginResponse resp;
+      req.set_username(userName);
+      req.set_password(password);
+
+      status = client.login(req, &resp);
+      std::cout << resp.loginticket() << std::endl;
+    } else if (func == "checkLogin") {
+      string userName(argv[i + 2]);
+      string loginTicket(argv[i + 3]);
+
+      UserCheckLoginRequest req;
+      UserCheckLoginResponse resp;
+      req.set_username(userName);
+      req.set_loginticket(loginTicket);
+
+      status = client.checkLogin(req, &resp);
+      std::cout << resp.sessionkey() << std::endl;
+    } else if (func == "logout") {
+      string userName(argv[i + 2]);
+      string sessionKey(argv[i + 3]);
+
+      UserLogoutRequest req;
+      auto baseReq = req.mutable_basereq();
+      UserLogoutResponse resp;
+      baseReq->set_username(userName);
+      baseReq->set_sessionkey(sessionKey);
+
+      status = client.logout(req, &resp);
     }
   }
 
